4cd7ec201b32e4e7e3329e609e96c9f0e76af263
frenzie24 <kevincharlesgross@gmail.com>
2024-04-26 19:12:38 -0400

    finished researching issue with prompts of type editor - users have to save the temporary file when they close added comments to startPrompt() in index.js created new function to format section enteries into markup formatted section strings called formatSection()

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const colors = require("colors");
 const fs = require('fs');
 
 
+const badges = [`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+    `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`,
+    `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
+    `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`,
+    `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`,
+    `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,];
 
+const choices = ['apache', 'boost', 'bsd3', 'bsd2', 'creative commons 0', 'MIT'];
 
 const addContentPrompt = {
     type: "confirm",
@@ -14,21 +21,51 @@ const addContentPrompt = {
 const contentPrompt = {
     type: "input",
     name: "content",
-    message: `Enter new line of content`
+    message: `Compose your content for this section`
 }
 
 const installPrompt = {
-    type: 'confirm',
-    name: 'install',
-    message: 'Are there any installation steps for your project?'
-};
-
-
-const installMorePrompt = {
     type: 'input',
-    name: 'installMore',
-    message: 'Are there any more installation steps for your project?'
+    name: 'install',
+    message: 'Please list any installation steps'
 };
+
+const usagePrompt = {
+    type: 'input',
+    name: 'usage',
+    message: 'What is your projects intended usage?',
+}
+
+const licensePrompt = {
+    type: 'list',
+    name: 'license',
+    message: 'Please select the license you will be using for this project',
+    choices: choices,
+}
+
+const contributingPrompt = {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please list contribution guidelines',
+}
+
+const testPrompt = {
+    type: "input",
+    name: "tests",
+    message: 'Please list any tests',
+}
+
+const questionsPrompot = {
+    type: "input",
+    name: "questions",
+    message: 'Please list any common questions may be answered.'
+}
+const githubPrompt = {
+    type: "input",
+    name: "github",
+    message: 'Please provide your github username.'
+}
+
 
 // in README markups two empty characters ('  ') at the end of the line indicates a new line
 // <br> can also be used in case the README view does not support ('  ') as a new line escape
@@ -36,21 +73,18 @@ const installMorePrompt = {
 const nl = `<br>`;
 
 // pass data obj which contains the heading as a string and the content as an array of strings
-let formatSections = (data) => {
 
-    let content = formatContent(data.content);
-
-    return `#${data.heading}${nl}${content}${nl}`;
-}
 
 // format content into markup and return
 let formatContent = (contentLines) => {
     let content = '';
-    contentLines.foreach(line => {
-        let lineString = JSON.stringify(line);
+    console.log(contentLines)
+    for (let i = 0; i < contentLines.length; i++) {
+        let lineString = JSON.stringify(contentLines[i]);
         // using string literals we can refractor the contentLines array 
         content = `${content}${lineString}${nl}`
-    });
+    }
+
     return content;
 }
 
@@ -63,7 +97,29 @@ let createContents = (sections) => {
     })
     return contents;
 }
+let emailPrompt = {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address"
+};
 
+let projectPrompt = {
+    type: "editor",
+    name: "projectName",
+    message: "What's the name of your project?",
+    validtate: (err) => { console.log('error validating editor') }
+};
+
+let descriptionPrompt = {
+    type: "input",
+    name: "descripton",
+    message: "Please decrible your project",
+}
+
+let formatSection = (section) => {
+    return `#${section.heading}${nl}${section.content}`;
+
+}
 
 //first function called to begin taking user input to create README
 async function startPrompt() {
@@ -73,74 +129,90 @@ async function startPrompt() {
         formattedData: ''
     };
 
-    let startPrompt = [{
-        type: "input",
-        name: "projectName",
-        message: "What's the name of your project?",
-    }, {
-        type: "input",
-        name: "descripton",
-        message: "Please decrible your project",
-    }, {
-        type: "confirm",
-        name: "incomplete",
-        message: "Would you like to make some sections?",
-
-    }];
+    let prompts = [projectPrompt, descriptionPrompt, installPrompt,
+        usagePrompt,
+        licensePrompt,
+        contributingPrompt,
+        testPrompt,
+        questionsPrompot,
+        githubPrompt,
+        emailPrompt,
+    ];
     // object to store readme data while we prep it for storing
 
     //
-    README.sections = await inquirer.prompt(startPrompt).then(response => {
-        let rdme = { project: response.name };
-        let sections = [];
-        // README.sections.push({ heading: `${response.heading} Description`, content: response.description });
+   
 
-        if (response.incomplete) {
-            sections.push(generateSectionA());
-        }
-        debugger;
+    await inquirer.prompt(prompts).then(response => {
+        let markupString = '';
+        let badge = badges[response.license.indexOf(choices.find((choice) => choice == response.license))];
+        let answers = [
+            { heading: response.projectName.toUpperCase(), content: response.description },
+            { heading: "INSTALLATION", content: response.install },
+            { heading: "USAGE", content: response.usage },
+            { heading: "LICENSE", content: response.license },
+            { heading: "CONTRIBUTING GUIDELINES", content: response.contributing },
+            { heading: "TEST INSTRUCTIONS", content: response.tests },
+            { heading: "GITHUB", content: response.github },
+            { heading: "EMAIL", content: response.email }
+        ]
+        console.log(answers);
+        answers.forEach(answer => {
+            // formatSection takes an answer object and creates a string literal to serve as the entire heading and content of a section
+            markupString = `${markupString}${formatSection(answer)}`
+        })
+        // f
+        markupString =`${badge}${markupString}`;
+        console.log(markupString);
+
+
     })
+
+    /*let rdme = { project: response.name };
+    let sections = [];
+    // README.sections.push({ heading: `${response.heading} Description`, content: response.description });
+
+    if (response.incomplete) {
+        sections = generateSection();
+        console.log
+    }
+    debugger;*/
+
 
 
 }
 
-async function generateSection() {
+async function generateSection(sections) {
     let sectionPrompt = [{
         type: 'input',
         name: 'heading',
         message: "Please enter the heading name for this section"
-    }, {
-        type: "editor",
-        name: "content",
-        message: `Please enter first line of content`
+    }, contentPrompt, {
+        type: 'confirm',
+        name: 'addSections',
+        message: 'Would you like to add more sections?'
     }]
 
 
     // content is an arrary of prompt responses that will be formatted in formatContent(commentLines)
-   
-    return await inquirer.prompt(sectionPrompt).then(response => {
-        // set content to resopnse.content
-        let heading = response.heading;
-        let content = [(`${response.content}${nl}`)];
-        // push response.content onto the content array. if they user wishes to continue call addContent
-
-        if (response.incomplete == true) {
-
-            return {heading: heading, content: content.push(addContent(content, contentPrompt))}
-          
-        } else {
-            return  {heading: heading, content: content};
-        }
-
-        console.log('content:', content);
-        console.log("section", { heading: heading, content: content })
-        return { heading: heading, content: content };
-        //ask the user if they want to add more lines to the content section
-        /*  inquirer.prompt(addContentPrompt).then(response => {
-             
-          });*/
-    });
+    if (!sections) {
+        sections = [];
+    }
+    const response = await inquirer.prompt(sectionPrompt);
+    const heading = response.heading;
+    const content = [`${response.content}${nl}`];
+    const section = { heading, content };
+    sections.push(section);
+    console.log(response)
+    if (response.addSections) {
+        return generateSection(sections);
+    } else {
+        console.log('Returning sections:');
+        console.log(sections);
+        return sections;
+    }
 }
+
 
 
 //function will call itself if the user wants multiple lines in their content section
---
+++ test.js
@@ -0,0 +1,24 @@
+/**
+ * Editor prompt example
+ */
+
+const inquirer = require("inquirer")
+const questions = [
+  {
+    type: 'editor',
+    name: 'bio',
+    message: 'Please write a short bio of at least 3 lines.',
+    validate(text) {
+      if (text.split('\n').length < 3) {
+        return 'Must be at least 3 lines.';
+      }
+
+      return true;
+    },
+    waitUserInput: true,
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, '  '));
+});
